dfb4ad16e70784c59d88ea5011d55fbd82c5ce1b
Albert <albert.gomes@liferay.com>
2025-02-21 14:58:57 -0300

    Create BenefitResource to represent metaData as a model

---
+++ src/main/java/com/associate/benefit/model/BenefitResources.java
@@ -0,0 +1,87 @@
+package com.associate.benefit.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
+
+/**
+ * @author Albert Gomes Cabral
+ */
+@Entity
+public class BenefitResources {
+
+    public Long getBenefitResourcesId() {
+        return benefitResourcesId;
+    }
+
+    public long getBenefitId() {
+        return benefitId;
+    }
+
+    public void setBenefitId(long benefitId) {
+        this.benefitId = benefitId;
+    }
+
+    public String getBenefitName() {
+        return benefitName;
+    }
+
+    public void setBenefitName(String benefitName) {
+        this.benefitName = benefitName;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getMetaData() {
+        return metaData;
+    }
+
+    public void setMetaData(String metaData) {
+        this.metaData = metaData;
+    }
+
+    public Map<String, String> getMetaDataMap() {
+        Map<String, String> metaDataMap = new HashMap<>();
+
+        JSONObject jsonObject = new JSONObject(metaData);
+
+        for (String key : jsonObject.keySet()) {
+            JSONObject object = jsonObject.getJSONObject(key);
+
+            metaDataMap.put(key, object.toString());
+        }
+
+        return metaDataMap;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long benefitResourcesId;
+
+    @Column(nullable = false)
+    private long benefitId;
+
+    @Column(nullable = false)
+    private String benefitName;
+
+    @Column(nullable = false)
+    private Date createDate;
+
+    @Column(nullable = false, length = 50000)
+    private String metaData;
+
+}
--- src/main/java/com/associate/benefit/persistence/BenefitResourcesPersistence.java
+++ src/main/java/com/associate/benefit/persistence/BenefitResourcesPersistence.java
@@ -1,4 +1,20 @@
 package com.associate.benefit.persistence;
 
-public interface BenefitResourcesPersistence {
+import com.associate.benefit.model.BenefitResources;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @author Albert Gomes Cabral
+ */
+@Repository
+public interface BenefitResourcesPersistence extends CrudRepository<BenefitResources, Long> {
+
+    List<BenefitResources> findByBenefitId(long benefitId);
+
+    BenefitResources findByMetaDataIsLike(String metaDataLike);
+
 }
