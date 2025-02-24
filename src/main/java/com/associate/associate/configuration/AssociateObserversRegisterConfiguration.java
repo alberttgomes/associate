c08d9815864510ed4faf66292cf2abe6445ef35b
Albert <albert.gomes@liferay.com>
2025-02-24 10:30:23 -0300

    Just adding the associates listed by company registared on company configs

--- src/main/java/com/associate/associate/configuration/AssociateObserversRegisterConfiguration.java
+++ src/main/java/com/associate/associate/configuration/AssociateObserversRegisterConfiguration.java
@@ -3,11 +3,13 @@ package com.associate.associate.configuration;
 import com.associate.associate.api.AssociateService;
 import com.associate.associate.model.Associate;
 import com.associate.associate.util.AssociateObserver;
+import com.associate.company.model.Company;
 import com.associate.notify.util.NotifySubject;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,15 +21,26 @@ public class AssociateObserversRegisterConfiguration {
 
     @Autowired
     public AssociateObserversRegisterConfiguration(
-            AssociateService associateService, NotifySubject notifySubject) {
+            AssociateService associateService,
+            @Qualifier("company") Company company, NotifySubject notifySubject) {
 
         this._associateService = associateService;
+        this._company = company;
         this._notifySubject = notifySubject;
     }
 
     @Bean
     public List<AssociateObserver> registerAssociates() {
-        for (Associate associate : _associateService.fetchAllAssociates()) {
+        long companyId = 0;
+
+        if (_company != null) {
+            companyId = _company.getCompanyId();
+        }
+
+        for (Associate associate :
+                _associateService.fetchAllAssociatesByCompanyId(
+                        companyId)) {
+
             _notifySubject.addObserver(associate);
         }
 
@@ -35,6 +48,8 @@ public class AssociateObserversRegisterConfiguration {
     }
 
     private final AssociateService _associateService;
+
+    private final Company _company;
 
     private final NotifySubject _notifySubject;
 
