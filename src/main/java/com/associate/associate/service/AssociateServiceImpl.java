dc4b0bb743a29e5d0a94e438de13afa8e9f1ae77
Albert <albert.gomes@liferay.com>
2025-02-26 01:28:41 -0300

    Remade message

--- src/main/java/com/associate/associate/service/AssociateServiceImpl.java
+++ src/main/java/com/associate/associate/service/AssociateServiceImpl.java
@@ -14,6 +14,7 @@ import com.associate.company.util.CompanyDynamicQuery;
 import com.associate.notify.model.Notify;
 import com.associate.notify.persistence.NotifyPersistence;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -100,7 +101,8 @@ public class AssociateServiceImpl implements AssociateService {
         }
 
         System.out.println(
-            "A new associate was added, but anything workflow process was sent.");
+                Instant.now() + " \\**[WARN]\\** A new associate was added," +
+                    " but anything workflow process was sent.");
 
         return addAssociate(
                 category, email, companyId, name, phoneNumber,
